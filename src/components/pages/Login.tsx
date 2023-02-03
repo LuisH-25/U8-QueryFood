554a93ccb54df4390a4b5b3ade8a36f62984642a
LuisH-25 <huanca.luis@pucp.pe>
2023-02-02 22:25:25 -0500

    add frontEnd and .env

---
+++ src/components/pages/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import FormLogin from "../sections/login/Login";
+
+function Login() {
+  return (
+    <>
+      <FormLogin />
+    </>
+  );
+}
+export default Login;
---
+++ src/components/pages/Restaurants.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Menu from "../sections/headingPages/Menu";
+
+function Restaurantes() {
+  return (
+    
+    <>
+        <main >
+          <Menu image={""} name={""} price={""} description={""}/>
+        </main> 
+    </>
+  );
+}
+export default Restaurantes;
